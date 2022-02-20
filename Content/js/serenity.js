e1ace2d71d0b723b707aa9024c514346f4bc4c7b
Wenyen333 <73871351+Wenyen333@users.noreply.github.com>
2022-02-20 00:32:03 -0800

    Add files via upload

--- Content/js/serenity.js
+++ Content/js/serenity.js
@@ -12,6 +12,22 @@ $(document).ready(function()
     });
 })
 
+function reveal() {
+    var reveals = document.querySelectorAll(".slide");
+  
+    for (var i = 0; i < reveals.length; i++) {
+      var windowHeight = window.innerHeight;
+      var elementTop = reveals[i].getBoundingClientRect().top;
+      var elementVisible = 150;
+  
+      if (elementTop < windowHeight - elementVisible) {
+        reveals[i].classList.add("active");
+      }
+    }
+  }
+  
+  window.addEventListener("scroll", reveal);
+
 
 function openProfileModal(person, backColor) {
     if(person == 'ceo')
