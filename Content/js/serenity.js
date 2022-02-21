85502c66e3f36a4f8c5cee0e0222d02572393379
Wenyen333 <73871351+Wenyen333@users.noreply.github.com>
2022-02-20 22:29:08 -0800

    Add files via upload

--- Content/js/serenity.js
+++ Content/js/serenity.js
@@ -13,24 +13,27 @@ $(document).ready(function()
 })
 
 function reveal() {
-    var reveals = document.querySelectorAll(".slide");
-  
-    for (var i = 0; i < reveals.length; i++) {
-      var windowHeight = window.innerHeight;
-      var elementTop = reveals[i].getBoundingClientRect().top;
-      var elementVisible = 150;
-  
-      if (elementTop < windowHeight - elementVisible) {
-        reveals[i].classList.add("active");
-      }
-    }
-  }
-  
-  window.addEventListener("scroll", reveal);
+    var reveals = document.querySelectorAll(".slide, .tokenomics-box, .extend-lines, .serenity-dots");
 
+    for (var i = 0; i < reveals.length; i++) {
+        var windowHeight = window.innerHeight;
+        var elementTop = reveals[i].getBoundingClientRect().top;
+        var elementVisible = 150;
+
+        if (elementTop < windowHeight - elementVisible) {
+        reveals[i].classList.add("active");
+        }
+    }
+}
+  
+window.addEventListener("scroll", reveal);
+
+function openVideo() {
+    $("#videoModal").modal('show');
+}
 
 function openProfileModal(person, backColor) {
-    if(person == 'ceo')
+    if(person == 'creative_director')
     {
         $("#profileModal .profile-photo .photo").html("<img src='Content/Images/profile_pic_fun_" + person + ".png'>");
         $("#profileModal .profile-photo .profile-info").html(
